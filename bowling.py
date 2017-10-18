db236b85c67ee5c1341e1af625d9be060ebb58d4
Dmytro Gierman <mryakzp@gmail.com>
2017-10-18 09:08:02 +0300

    test for Player.play_frame()

--- bowling.py
+++ bowling.py
@@ -19,6 +19,16 @@ class Player:
         self.rolls.append(result)
         return result
 
+    def play_frame(self):
+        """
+        The player rolls a ball one (if strike) or two times.
+        Results are saved in self.frames;
+        The tenth frame could be three rolls long to satisfy
+        spare or strike bonus (see bowling rules)
+        """
+        frame = []
+
+
 
 class Game:
     def __init__(self, *names):
--- bowling_tests.py
+++ bowling_tests.py
@@ -18,6 +18,33 @@ class TestPlayer(unittest.TestCase):
             )
             self.assertEqual(len(p.rolls), i+1)
 
+    def testPlayFrame(self):
+        """
+        Method should append a valid frame to the player.frames:
+        [10]  # strike
+        [6, 0]  # usual frame
+        [x, x] or [x,x,x]  # tenth frame
+        """
+        for _ in range(200):
+            p = Player()
+            for i in range(9):
+                p.play_frame()
+                frame = p.frames[i]
+                self.assertTrue(type(frame) == list)
+                self.assertTrue(1 <= len(frame) <= 2)
+                self.assertTrue(0 <= sum(frame) <= 10)
+                if len(frame) == 1:
+                    self.assertEqual(frame, [10])
+                # check frames are stored
+                self.assertEqual(len(p.frames), i+1)
+                self.assertTrue(p.frames[i] is frame)
+            # last frame
+            p.play_frame()
+            frame = p.frames[9]
+            self.assertTrue(2 <= len(frame) <= 3)
+            self.assertTrue(0 <= sum(frame) <= 30)
+            self.assertEqual(len(p.frames), 10)
+
 
 class TestGame(unittest.TestCase):
     pass
